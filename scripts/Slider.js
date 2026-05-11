01c45ab0fb7e010f3a9562cde90548f4f87979fa
Artem <artwhite2012@gmail.com>
2026-05-11 16:02:01 +0300

    Add timer reset in condition check which trace if dots been created

--- scripts/Slider.js
+++ scripts/Slider.js
@@ -239,6 +239,7 @@ export class Slider {
         this.#currentSlideIndex = targetIndex;
         this.#updateSlidePosition();
         this.#updateDots();
+        this.#resetTimer();
       });
     }
 
